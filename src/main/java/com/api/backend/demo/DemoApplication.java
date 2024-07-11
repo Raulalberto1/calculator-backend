3337f993e228250e3201c4c3619ec7d072948a8a
Raulalberto1 <raul.alberto.91@gmail.com>
2024-07-11 00:17:56 -0600

    Cambios

--- src/main/java/com/api/backend/demo/DemoApplication.java
+++ src/main/java/com/api/backend/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.api.backend.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,4 +14,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
